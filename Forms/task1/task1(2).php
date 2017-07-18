0a7ba8650b0efc5b1e8210b707ed7559a7339fe9
Оля <klekot.olga@gmail.com>
2017-07-18 22:22:43 +0300

    Simple Form with PHP Response

---
+++ Forms/task1/task1(2).php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Form</title>
+    <style>
+        .city {
+            color: orangered;
+            font-size: larger;
+            margin: 5px;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        $city = $_POST['favoriteCity'];
+    ?>
+
+    <h3>Your favorite city is
+        <span class="city"><?= $city ?></span>!
+    </h3>
+
+</body>
+</html>
