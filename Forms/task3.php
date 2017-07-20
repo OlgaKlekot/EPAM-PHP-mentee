25c4bd7b871e87ff62714534163fa338162d06ed
Оля <klekot.olga@gmail.com>
2017-07-20 13:06:57 +0300

    If-Elseif-Else Construction/дубль 2

--- Forms/task3.php
+++ Forms/task3.php
@@ -29,43 +29,46 @@
     <p>
 
 <?php
-$day = ucfirst($_POST['today']);
-$daySpan = 'Laugh on <span class="day">' . $day . '</span>,';
-if (!isset($_POST['today']) || (isset($_POST['today']) && ($day == null))): ?>
+if (isset($_POST['today'])) {
+    $day = ucfirst($_POST['today']);
+    $daySpan = 'Laugh on <span class="day">' . $day . '</span>,';
+
+    if ($day == 'Monday') { ?>
+    <?= $daySpan ?> laugh for danger.
+
+<?php } elseif ($day == 'Tuesday') { ?>
+    <?= $daySpan ?> kiss a stranger.
+
+<?php } elseif ($day == 'Wednesday') { ?>
+    <?= $daySpan ?> laugh for a letter.
+
+<?php } elseif ($day == 'Thursday') { ?>
+    <?= $daySpan ?> something better.
+
+<?php } elseif ($day == 'Friday') { ?>
+    <?= $daySpan ?> laugh for sorrow.
+
+<?php } elseif ($day == 'Saturday') { ?>
+    <?= $daySpan ?> joy tomorrow.
+
+<?php } elseif ($day == 'Sunday') { ?>
+    Come tomorrow =)
+
+<?php } else { ?>
+        Incorrectly!
+
+<?php }
+
+} else { ?>
 
     <form method="post" id="formDay">
         <label for="day">Enter a day of the week! </label>
-        <input type="text" id="day" name="today" value="">
+        <input type="text" id="day" name="today">
         <br>
         <input type="submit" name="submit" value="Ok">
     </form>
 
-
-<?php elseif ($day == 'Monday'): ?>
-    <?= $daySpan ?> laugh for danger.
-
-<?php elseif ($day == 'Tuesday'): ?>
-    <?= $daySpan ?> kiss a stranger.
-
-<?php elseif ($day == 'Wednesday'): ?>
-    <?= $daySpan ?> laugh for a letter.
-
-<?php elseif ($day == 'Thursday'): ?>
-    <?= $daySpan ?> something better.
-
-<?php elseif ($day == 'Friday'): ?>
-    <?= $daySpan ?> laugh for sorrow.
-
-<?php elseif ($day == 'Saturday'): ?>
-    <?= $daySpan ?> joy tomorrow.
-
-<?php elseif ($day == 'Sunday'): ?>
-    Come tomorrow =)
-
-<?php else: ?>
-    Incorrectly!
-
-<?php endif; ?>
+<?php } ?>
 
     </p>
 </body>
